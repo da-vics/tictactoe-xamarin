c2fcc98b59a123ebaad10086b2d47bac0e16cbae
Tomoloju Victor <tomoloju45@gmail.com>
2020-09-25 23:41:25 -1100

    custom text font

--- xamarin tictactoe/xamarin tictactoe/AssemblyInfo.cs
+++ xamarin tictactoe/xamarin tictactoe/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 [assembly: ExportFont("fa-solid-900.ttf", Alias = "FontAwesome")]
+[assembly: ExportFont("TrashHand.ttf", Alias = "HandFont")]
